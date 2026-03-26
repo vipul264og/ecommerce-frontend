d00c76a1565653bde6965f4d365791ec9fc619e2
vipul <2400031116@kluniversity.in>
2026-03-26 10:29:19 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api"
+  baseURL: "ecommercebackend-production-e850.up.railway.app/api"
 });
 
 export default API;
